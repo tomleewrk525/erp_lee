1d82c5e7bc7bb41082cb19355577f5f8f40ce191
tomleewrk525 <tomleewrk525@gmail.com>
2025-12-25 03:21:12 +0700

    feat(auth): Add shadergradient to authentication page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,8 +1,22 @@
+'use client';
+
 import AuthForm from "@/components/auth/auth-form";
+import { ShaderGradient } from 'shadergradient';
 
 export default function AuthenticationPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="relative flex items-center justify-center h-screen">
+      <ShaderGradient
+        cAzimuthAngle={180}
+        cPolarAngle={90}
+        cDistance={2.7}
+        color1="#A69080"
+        color2="#D9AAB3"
+        color3="#8C7D85"
+        range="disabled"
+        uSpeed={0.4}
+        className="absolute inset-0 -z-10"
+      />
       <AuthForm />
     </div>
   );
